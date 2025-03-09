88a3229d6cd0094a143305700dbdedc4125c6621
Hannes Vieth <hannesvieth@gmail.com>
2025-03-09 06:35:16 +0200

    viedään tuotantoon validointiharjoitusten ja monen muun jälkeen

--- helloworld/index.js
+++ helloworld/index.js
@@ -1,16 +1,12 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const mongoose = require('mongoose')
 const Note = require('./models/note')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
-
-mongoose.set('strictQuery',false)
-
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
@@ -22,21 +18,38 @@ app.get('/api/notes', (request, response) => {
     })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    const note = notes.find(note => note.id === id)
-    if (note) {
-        response.json(note)
-      } else {
-        response.status(404).end()
-      }
-  })
+app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id)
+        .then(note => {
+            if (note) {
+                response.json(note)
+            } else {
+                response.status(404).end()
+            }
+    })
+        .catch(error => next(error))
+        })
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    notes = notes.filter(note => note.id !== id)
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+    const { content, important } = request.body
   
-    response.status(204).end()
+    Note.findByIdAndUpdate(request.params.id,
+        { content, important },
+        { new: true, runValidators: true, context: 'query' }
+    )
+    .then(updatedNote => {
+        response.json(updatedNote)
+    })
+    .catch(error => next(error))
+    
   })
 
 const generateId = () => {
@@ -46,25 +59,45 @@ const generateId = () => {
     return String(maxId + 1)
 }
 
-app.post('/api/notes', (request, response) => {
-    const body = request.body
+app.post('/api/notes', (request, response, next) => {
+  const body = request.body
 
-    if (!body.content) {
-        return response.status(400).json({ 
-            error: 'content missing' 
-        })
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+  .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+  // olemattomien osoitteiden käsittely
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
-
-    const note = {
-        content: body.content,
-        important: body.important || false,
-        id: generateId(),
-    }
-
-    notes = notes.concat(note)
-
-    response.json(note)
-    })
+  
+    next(error)
+  }
+  
+  // tämä tulee kaikkien muiden middlewarejen ja routejen rekisteröinnin jälkeen!
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
